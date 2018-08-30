7cee879765166450a3735ddfc0725cef1b953667
jgauna <julian.gauna@fluxit.com.ar>
2018-08-30 09:16:46 -0300

    programa funcionando con vista

--- www/app.py
+++ www/app.py
@@ -20,7 +20,7 @@ def index():
     proc.start_process()
     samples = db.get_last_ten()
     promedios = db.get_promedio()
-    return render_template('index.html', samples = samples, promedios = promedios, refresco = 1)
+    return render_template('index.html', samples = samples, promedios = promedios, refresco = 10)
 
 @app.route("/actualizar", methods=["POST"])
 def actualizar():
